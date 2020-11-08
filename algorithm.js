10a089b19284e74f1524a1522dc395ce52593007
tobymould <toby.mould@icloud.com>
2020-11-08 18:34:50 +0000

    cleaned up the 'cleanDBData' & 'cleanImgData' by A) converting the label filtering code into its own function called 'filterLabelsArray()' (to only keep the 'description' and 'score' values for each label for each image) and B) converting the label removal code into its own function called 'removeLowAccuracyLabels()'' (to remove any labels from each image object that have a score < 0.84) - also limited each 'score' for each label to 3-decimal places inside the 'filterLabelsArray' function

--- algorithm.js
+++ algorithm.js
@@ -5,11 +5,13 @@ const lookUpTable = [
     generalCategory: ['Fruit', 'Vegetable', "Flowering plant", 'Tree', 'Cut flowers']
   }, 
   {
-    specificCategory: ['Yellow', 'Green algae', 'Green', 'Banana', 'Purple','Red', 'Pink', 'White', 'Rose']
+    colourCategory: ['Yellow', 'Green algae', 'Green', 'Banana', 'Purple','Red', 'Pink', 'White', 'Rose']
   }
 ];
 
+// -------------------------------------------------------------------------------
 // 1) RAW FUNCTIONS
+// -------------------------------------------------------------------------------
 const getJsonData = async (url) => {
   const response = await fetch(`./${url}`);
   errorHandler(response, url);
@@ -18,47 +20,46 @@ const getJsonData = async (url) => {
   return data;
 }
 
+const removeLowAccuracyLabels = (imageObject) => {
+  console.log('test: ', imageObject);
+
+  imageObject.labelsArray.map(labelObject => {
+    Object.keys(labelObject)
+    .filter(key => labelObject.score < 0.8)
+    .forEach(key => delete labelObject[key])
+    
+  });
+
+  console.log('test5: ', imageObject);
+  return imageObject;
+}
+
+const filterLabelsArray = (imageObject) => {
+  const filteredLabelsArray = imageObject.labelsArray.map((labelObject => {
+    labelObject.score = labelObject.score.toFixed(2);
+    return {'description': labelObject.description, 'score': labelObject.score}
+  }))
+  const imageObjectFiltered = {imageName: 'submittedImage', labelsArray: filteredLabelsArray}
+  console.log('test9: ', imageObjectFiltered)
+
+  return imageObjectFiltered;
+}
+
 const cleanDBData = (data) => {
   const arrayOfImageDataObjects = Object.entries(data).map((subArray) => {
     return {imageName: subArray[0], labelsArray: subArray[1]}
   }) 
-  // console.log('arrayOfImageDataObjectsA', arrayOfImageDataObjects)
   // C) Iterate the 'labelsArray' nested-array, and delete all properties which are not 'description' or 'score' (because useless).
   
-  arrayOfImageDataObjects.forEach((imageObject => {
-    const allowed = ['description', 'score'];
-
-    const result = imageObject.labelsArray.forEach(labelObject => {
-      Object.keys(labelObject)
-      .filter(key => !allowed.includes(key))
-      .forEach(key => delete labelObject[key])        
-      // return {'description': labelObject.description, 'score': labelObject.score}
-    })
-
-    // return {imageName: imageObject[0], labelsArray: result}
-    return result;
+  arrayOfImageDataObjects.forEach(((imageObject, index) => {
+    const imageObjectFiltered = filterLabelsArray(imageObject);
+    return arrayOfImageDataObjects[index] = imageObjectFiltered
   }))
-
-  // console.log('Filtered Object: ', arrayOfImageDataObjects)
 
   // D) Iterate the 'labelsArray' nested-array again, and delete all objects with an accuracy score < 84% (because low accuracy match).
   arrayOfImageDataObjects.forEach((imageObject => {
-    const result = imageObject.labelsArray.map(labelObject => {
-      const test = Object.keys(labelObject)
-      .filter(key => labelObject.score < 0.84)
-      .forEach(key => delete labelObject[key])
-    })
-    
-    return result
+    removeLowAccuracyLabels(imageObject)
   }))
-
-  // arrayOfImageDataObjects.forEach((imageObject => {
-  //   const result = imageObject.labelsArray.map(labelObject => {
-  //     const test = Object.keys(labelObject)
-  //     .filter(key => Object.keys(labelObject).length !== 0)
-  //     .forEach(key => delete labelObject[key])
-  //   })
-  // }))
 
   // E) Return the cleaned data
   return arrayOfImageDataObjects;
@@ -66,14 +67,15 @@ const cleanDBData = (data) => {
 
 const cleanImgData = (data) => {
 const isolatedLabelObject = Object.entries(data).find((subArrays) => subArrays.includes("labelAnnotations"));
-const destructuredObject1 = {imageName: 'submittedImage', labelsArray: isolatedLabelObject[1]} 
+const imageObject = {imageName: 'submittedImage', labelsArray: isolatedLabelObject[1]} 
 
 // C) Iterate the 'labelsArray' nested-array, and delete all properties which are not 'description' or 'score' (because useless).
-const filteredLabelsArray = destructuredObject1.labelsArray.map((labelObject => {return {'description': labelObject.description, 'score': labelObject.score}}))
-const destructuredObject2 = {imageName: 'submittedImage', labelsArray: filteredLabelsArray} 
+const imageObjectFiltered = filterLabelsArray(imageObject);
 
-// console.log('destructuredObject2: ', destructuredObject2);
-return destructuredObject2;
+removeLowAccuracyLabels(imageObjectFiltered);
+
+// console.log('imageObjectFiltered: ', imageObjectFiltered);
+return imageObjectFiltered;
 }
 
 const errorHandler = (response, msg) => {
@@ -83,13 +85,14 @@ if (response.status !== 200) {
 }
 
 const broadCategoryCheckAlgorithm = (dataFromSubmitted) => {
+  let categoryMatched;
 
   const comparisonResult = dataFromSubmitted.labelsArray.forEach(labelObject => {
     return lookUpTable[0].generalCategory.forEach(category => 
       labelObject.description === category ? categoryMatched = category : null
     )
   })
-
+  
   return categoryMatched;
 }
 
@@ -103,15 +106,11 @@ const BroadMatchToDBImagesAlgorithm = (dataFromDB, categoryOfSubmittedImage) => 
   })
 
   return matchingImages;
-
 }
 
-const specificCategoryCheckerAlgorithm = () => {
-
-}
-
-
+// -------------------------------------------------------------------------------
 // 2) EXECUTION FUNCTIONS - FUNCTIONS THAT CALLOUT OTHER SIMPLER FUNCTIONS TO RUN.
+// -------------------------------------------------------------------------------
 
 const ProcessDataFromDB = async () => {
   const dataFromDB = await getJsonData('data.json')
@@ -120,33 +119,90 @@ const ProcessDataFromDB = async () => {
 }
 
 const ProcessDataFromImg = async () => {
-  const dataFromSubmittedImage = await getJsonData('testData.json')
+  const dataFromSubmittedImage = await getJsonData('testData2.json')
   const cleanedDataImg = cleanImgData(dataFromSubmittedImage);
   return cleanedDataImg
 }
 
 const runTheComparision = async () => {
     try {
-      const dataFromDB = await ProcessDataFromDB();
       const dataFromSubmitted = await ProcessDataFromImg();
+      const dataFromDB = await ProcessDataFromDB();
 
       const submittedImageBroadCategoryMatch = broadCategoryCheckAlgorithm(dataFromSubmitted);
       const imageMatch = BroadMatchToDBImagesAlgorithm(dataFromDB,submittedImageBroadCategoryMatch);
 
       // imageMatch.length > 3 ? specificCategoryCheckerAlgorithm() : null;
       
-      console.log('imageMatch: ', imageMatch);
-      console.log('imageMatch: ', imageMatch);
-      console.log('submittedImageBroadCategoryMatch: ', submittedImageBroadCategoryMatch);
       console.log('dataFromDB: ', dataFromDB);
       console.log('dataFromSubmitted: ', dataFromSubmitted);
+      console.log('submittedImageBroadCategoryMatch: ', submittedImageBroadCategoryMatch);
+      console.log('imageMatch: ', imageMatch);
 
 
     } catch(err){
       console.log('rejectedDB: ', err.message)
     };
 
+
 }
 
 // 3) STARTS THE SCRIPT
 runTheComparision();
+
+
+
+// 
+// const allowed = ['description', 'score'];
+
+    // const result = imageObject.labelsArray.forEach(labelObject => {
+    //   // console.log('labelObject.score-BEFORE', labelObject.score);
+    //   labelObject.score = labelObject.score.toFixed(2);
+    //   // console.log('labelObject.score-AFTER', labelObject.score);
+
+    //   Object.keys(labelObject)
+    //   .filter(key => !allowed.includes(key))
+    //   .forEach(key => delete labelObject[key])        
+    //   // return {'description': labelObject.description, 'score': labelObject.score}
+    // })
+    // console.log('imageObject-before', imageObject);
+    // console.log('LabelsArray-before', imageObject.labelsArray);
+
+
+
+
+    // const filteredLabelsArray = imageObject.labelsArray.map((labelObject => {
+    //   labelObject.score = labelObject.score.toFixed(2);
+    //   return {'description': labelObject.description, 'score': labelObject.score}
+    // }))
+
+    // // console.log('filteredLabelsArray-after', filteredLabelsArray);
+    // imageObject = {imageName: 'submittedImage', labelsArray: filteredLabelsArray} 
+    // console.log('imageObject-after', imageObject)
+    // imageObject = imageObjectFiltered;
+    // return {imageName: imageObject[0], labelsArray: result}
+    // return imageObject;
+    // return arrayOfImageDataObjects[index] = {imageName: 'submittedImage', labelsArray: imageObjectFiltered}
+
+
+
+
+      // if (arrayOfImageDataObjects === undefined) {
+  //   throw new Error(`not working`);
+  // } 
+
+  // arrayOfImageDataObjects.forEach((imageObject => {
+  //   const result = imageObject.labelsArray.map(labelObject => {
+  //     const test = Object.keys(labelObject)
+  //     .filter(key => Object.keys(labelObject).length !== 0)
+  //     .forEach(key => delete labelObject[key])
+  //   })
+  // }))
+
+      // imageObject.labelsArray.forEach(labelObject => {
+    //   Object.keys(labelObject)
+    //   .filter(key => labelObject.score < 0.8)
+    //   .forEach(key => delete labelObject[key])
+      
+    // });
+    // return
